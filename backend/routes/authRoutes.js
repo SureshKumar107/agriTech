128d037b86f0b7d0bdfbdaf522a5820a4cf22448
Suresh Kumar <142878149+skb8055@users.noreply.github.com>
2024-12-23 19:53:06 +0530

    backend changes

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/sign-up', registerUser);
-router.post('/sign-in', loginUser); 
-
-module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import WeatherPrediction from "./pages/WeatherPrediction";
 import CropGuide from "./pages/CropGuide";
 import RecentNews from "./pages/RecentNews";
-import MarketPlace from "./pages/MarketPlace";
 import AboutUs from "./pages/AboutUs";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import TradingPage from "./pages/TradingPage";
 import Footer from "./components/Footer";
-import ContactUs from "./pages/ContactUs";
 import ExpertSupport from "./additional pages/ExpertSupport";
 import DetailedForecast from "./additional pages/DetailedForecast";
 import ContactExperts from "./additional pages/ContactExperts";
@@ -25,12 +24,10 @@ function App() {
         <Route path="/weather-prediction" element={<WeatherPrediction />} />
         <Route path="/crop-guide" element={<CropGuide />} />
         <Route path="/recent-news" element={<RecentNews />} />
-        <Route path="/market-place" element={<MarketPlace />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/tradepage" element={<TradingPage/>}/>
-        <Route path="/contact-us" element={<ContactUs/>}/>
         <Route path="/expert-support" element={<ExpertSupport/>}/>
         <Route path="/detailed-forecast" element={<DetailedForecast/>}/>
         <Route path="/contact-experts" element={<ContactExperts/>}/>
